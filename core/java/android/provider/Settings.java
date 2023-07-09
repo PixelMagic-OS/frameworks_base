234dcb68091b67d2891f5d2d52d01ed3aad2ff52
Magicxavi <rolfpeterkesche@gmail.com>
2023-07-09 07:35:13 +0300

    base: drop LS support from weather service API

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6092,18 +6092,6 @@ public final class Settings {
          */
         public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
 
-	/**
-         * Keyguard weather temperature
-         * 0: Hide the temperature
-         * 1: Display the temperature with scale and image
-         * 2: Display the temperature without scale and with image
-         * 3: Display the temparature with scale and without image
-         * 4: Display the temperature without scale and without image
-         * 5: Display the temperature image
-         * @hide
-         */
-	public static final String KG_SHOW_WEATHER_TEMP = "kg_show_weather_temp";
-
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -6286,7 +6274,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_ENABLED);
             PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_TIMEOUT);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
-	        PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
         }
         /**
          * These entries are considered common between the personal and the managed profile,
