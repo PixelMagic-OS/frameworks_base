5962da8dae0d56c5585192f6ca51720fc8badb8f
Dhina17 <dhinalogu@gmail.com>
2024-01-30 05:30:14 +0000

    SystemUI: Make sure the udfps animation is hidden on the bouncer
    When pressing the udfps icon repetively for some time, it will enter primary bouncer.
    While launching bouncer, Somehow touch event ACTION_DOWN passes the checks in newOnTouch()
    and invokes onFingerDown(). So it shows udfps animation.
    on next event, checks in newOnTouch() stop it and not able to reach onFingerUp().
    So unable to hide udfps animation until you exit bouncer and tap the screen once.
    
    To avoid this case, hide the udfps animation in all checks of newOnTouch().
    
    [@neobuddy89: Add showUdfpsAnimation() similarly.]
    
    Change-Id: I629a1b4e0a1c45814cc8470315c29a44cb46a0c9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -343,18 +343,14 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         for (Callback cb : mCallbacks) {
                             cb.onFingerDown();
                         }
-                        if (mUdfpsAnimation != null) {
-                            mUdfpsAnimation.show();
-                        }
+                        showUdfpsAnimation();
                     });
                 } else {
                     mFgExecutor.execute(() -> {
                         for (Callback cb : mCallbacks) {
                             cb.onFingerUp();
                         }
-                        if (mUdfpsAnimation != null) {
-                            mUdfpsAnimation.hide();
-                        }
+                        hideUdfpsAnimation();
                     });
                 }
             }
@@ -591,15 +587,18 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private boolean onTouch(long requestId, @NonNull MotionEvent event, boolean fromUdfpsView) {
         if (!fromUdfpsView) {
             Log.e(TAG, "ignoring the touch injected from outside of UdfpsView");
+            hideUdfpsAnimation();
             return false;
         }
         if (mOverlay == null) {
             Log.w(TAG, "ignoring onTouch with null overlay");
+            hideUdfpsAnimation();
             return false;
         }
         if (!mOverlay.matchesRequestId(requestId)) {
             Log.w(TAG, "ignoring stale touch event: " + requestId + " current: "
                     + mOverlay.getRequestId());
+            hideUdfpsAnimation();
             return false;
         }
         if (event.getAction() == MotionEvent.ACTION_DOWN
@@ -621,6 +620,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                     && !mAlternateBouncerInteractor.isVisibleState())
                     || mPrimaryBouncerInteractor.isInTransit()) {
                 Log.w(TAG, "ignoring touch due to qsDragProcess or primaryBouncerInteractor");
+                hideUdfpsAnimation();
                 return false;
             }
         }
@@ -629,6 +629,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 mOverlayParams);
         if (result instanceof TouchProcessorResult.Failure) {
             Log.w(TAG, ((TouchProcessorResult.Failure) result).getReason());
+            hideUdfpsAnimation();
             return false;
         }
 
@@ -1174,9 +1175,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             for (Callback cb : mCallbacks) {
                 cb.onFingerDown();
             }
-            if (mUdfpsAnimation != null) {
-                mUdfpsAnimation.show();
-            }
+            showUdfpsAnimation();
         }
     }
 
@@ -1217,9 +1216,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 for (Callback cb : mCallbacks) {
                     cb.onFingerUp();
                 }
-                if (mUdfpsAnimation != null) {
-                    mUdfpsAnimation.hide();
-                }
+                hideUdfpsAnimation();
             }
         }
         mOnFingerDown = false;
@@ -1229,6 +1226,18 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
     public boolean isAnimationEnabled() {
         return mUdfpsAnimation != null && mUdfpsAnimation.isAnimationEnabled();
+    }
+
+    private void showUdfpsAnimation() {
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.show();
+        }
+    }
+
+    private void hideUdfpsAnimation() {
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.hide();
+        }
     }
 
     /**
