da73f1c9d8b8048a075170ec16f670e4c6715883
buwenjing <buwenjing@xiaomi.com>
2024-08-24 17:50:16 +0800

    Ignore pip task when checking task visibility in recents transition
    Bug: 361847232
    Test: Manual test
    
    Change-Id: I0d1f689c48e34d8ecb56776bea898f3a89f93c7c
    Signed-off-by: buwenjing <buwenjing@xiaomi.com>

--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -2917,6 +2917,10 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
                 // (about to be visible).
                 return false;
             }
+            if (r.getTask() != null && r.getTask().inPinnedWindowingMode()) {
+                // Ignore pip activities.
+                return false;
+            }
             return r.occludesParent(mIncludeInvisibleAndFinishing /* includingFinishing */);
         }
     }
