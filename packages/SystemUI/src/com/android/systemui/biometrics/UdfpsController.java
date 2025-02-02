61327b176326025e07be84a78ec0438154175201
Pranav Vashi <neobuddy89@gmail.com>
2025-02-02 04:15:45 +0200

    UdfpsAnimation: Address memory leaks and improvements
    Ref: https://github.com/crdroidandroid/issue_tracker/issues/478
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -877,21 +877,21 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
 
         mUdfpsVendorCode = mContext.getResources().getInteger(R.integer.config_udfpsVendorCode);
-        updateUdfpsAnimation();
-        mConfigurationController.addCallback(mConfigurationListener);
+        if (com.android.internal.util.pm.Utils.isPackageInstalled(mContext,
+                "com.crdroid.udfps.animations")) {
+            updateUdfpsAnimation();
+            mConfigurationController.addCallback(mConfigurationListener);
+        }
     }
     
     private void updateUdfpsAnimation() {
-        if (com.android.internal.util.pm.Utils.isPackageInstalled(mContext,
-                "com.crdroid.udfps.animations")) {
-            if (mUdfpsAnimation != null) {
-                mUdfpsAnimation.removeAnimation();
-                mUdfpsAnimation = null;
-            }
-            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
-            if (mUdfpsAnimation != null) {
-                mUdfpsAnimation.updatePosition();
-            }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.removeAnimation();
+            mUdfpsAnimation = null;
+        }
+        mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.updatePosition();
         }
     }
 
