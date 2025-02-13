6ea3ffe45ea782dc79978ccb579c3df9e016b428
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-13 18:16:38 +0800

    [AxionUI] QuickSettings UI changes
    Change-Id: Ic25175b661764ea5aa2f801651963a90ed70d587
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -477,6 +477,11 @@ public class QSPanel extends LinearLayout implements Tunable {
             switchToParent(mFooter, parent, index);
             index++;
         }
+        
+        if (mBrightnessView != null) {
+            switchToParent(mBrightnessView, parent, index);
+            index++;
+        }
     }
 
     private void switchToParent(View child, ViewGroup parent, int index) {
